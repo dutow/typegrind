1f949de25fe858598e645267295c81fd74e01f0f
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2015-10-08 03:39:30 -0700

    added new example

---
+++ examples/default_new_array_typedef.cpp
@@ -0,0 +1,13 @@
+/*
+Important AST snippet:
+======================
+
+*/
+
+typedef int arrTypeDef[53];
+int main(void) {
+    int* pT = new arrTypeDef;
+    // expected
+    // TODO
+    return 0;
+}
